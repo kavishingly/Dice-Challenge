27f293c60afc16b521314d648d2489350c478381
Kavish Agrawal <63361734+kavishingly@users.noreply.github.com>
2021-09-28 00:23:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var dice2=document.querySelector("#dice-2");
 var p1=Math.floor((Math.random())*6)+1;
 var p2=Math.floor((Math.random())*6)+1;
     
-dice1.src="/resources/images/dice"+p1+".png";
+dice1.src="resources/images/dice"+p1+".png";
 // dice1.src="C:/Users/pc/OneDrive/Desktop/UDEMY FULL STACK WEB/Dice challenge/resources/images/dice"+p1+".png";
 // dice2.src="C:/Users/pc/OneDrive/Desktop/UDEMY FULL STACK WEB/Dice challenge/resources/images/dice"+p2+".png";
-dice2.src="/resources/images/dice"+p2+".png";
+dice2.src="resources/images/dice"+p2+".png";
 
 if(p1>p2){
     head.innerHTML='<i class="fas fa-trophy"></i> Player 1 wins ';
